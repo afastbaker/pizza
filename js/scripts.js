dd925c30d4e7d7892b8f03fc76e9580ffa030856
Alex Baker <afastbaker@gmail.com>
2018-03-31 10:30:17 -0700

    This will add the If Else statement to js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,20 @@ function Pizza(topping, size) {
 }
 Pizza.prototype.pizzaCost = function() {
   var cost = 0;
+  
+  if (this.topping === "topping1"){
+    cost += 1;
+  } else if (this.topping === "topping2") {
+    cost += 2;
+  } else if (this.topping === "topping3") {
+    cost += 3;
+  }
+  if (this.size === "small") {
+    cost += 5;
+  } else if (this.size === "medium") {
+    cost += 10;
+  } else if (this.size === "large") {
+    cost += 15;
+  }
+  return cost;
+}
